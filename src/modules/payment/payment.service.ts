d8dec67b47f9ae0e7c0439d135978187f0ea408e
Afsana-Jannat <afsanajannat205@gmail.com>
2026-08-01 01:35:09 +0600

    some

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -156,6 +156,14 @@ const getSinglePaymentFromDB = async (id: string, customerId: string) => {
 };
 
 const paymentSuccessIntoDB = async (sessionId: string) => {
+
+  console.log(
+  `${config.frontend_url}/payment/success?session_id={CHECKOUT_SESSION_ID}`
+);
+
+console.log(
+  `${config.frontend_url}/payment/cancel`
+);
   const session = await stripe.checkout.sessions.retrieve(sessionId);
 
   if (session.payment_status !== 'paid') {
