a09dd4e5bd86576ad6ead1a9acd8840279e86130
Afsana-Jannat <afsanajannat205@gmail.com>
2026-08-02 22:01:33 +0600

    interface add image

--- src/modules/category/category.interface.ts
+++ src/modules/category/category.interface.ts
@@ -1,9 +1,11 @@
 export interface ICreateCategory {
   name: string;
   description?: string;
+  image?: string;
 }
 
 export interface IUpdateCategory {
   name?: string;
   description?: string;
+  image?: string;
 }
