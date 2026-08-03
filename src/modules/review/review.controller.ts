c4424c5d4d9d13631573a06e7ad4a56f6ab64a42
Afsana-Jannat <afsanajannat205@gmail.com>
2026-08-03 16:23:34 +0600

    nk

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -19,6 +19,8 @@ const createReview = catchAsync(
   }
 );
 
+// getreviewsbygear
+
 const getReviewsByGear = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const reviews = await reviewService.getReviewsByGearFromDB(
